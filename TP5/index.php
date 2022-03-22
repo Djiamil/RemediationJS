b50077155b3dffc2f8ac62d2cbba6b5d0a9c1699
Moustapha Der <moustaphader61@gmail.com>
2022-03-22 19:59:49 +0000

    tp3

--- TP5/index.php
+++ TP5/index.php
@@ -18,51 +18,16 @@
     <main id="main">
         <div class="movie">
             <img src="" alt="" id="img">
-            <div movie-info>
-                <h3>Movie Title</h3>
-                <span class="green">9.8</span>
-            </div>
-            <div class="overview">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab tenetur quidem rerum ex voluptate ratione at, odio facere non expedita!
-
-            </div>
-        </div>
-
-        <div class="movie">
-            <img src="" alt="">
-            <div movie-info>
-                <h3>Movie Title</h3>
-                <span class="green">9.8</span>
-            </div>
-            <div class="overview">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab tenetur quidem rerum ex voluptate ratione at, odio facere non expedita!
-                
-            </div>
-        </div>
-
-        <div class="movie">
-            <img src="" alt="">
             <div class="movie-info">
                 <h3>Movie Title</h3>
                 <span class="green">9.8</span>
             </div>
             <div class="overview">
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab tenetur quidem rerum ex voluptate ratione at, odio facere non expedita!
-                
+
             </div>
         </div>
 
-        <div class="movie">
-            <img src="" alt="">
-            <div movie-info>
-                <h3>Movie Title</h3>
-                <span class="green">9.8</span>
-            </div>
-            <div class="overview">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab tenetur quidem rerum ex voluptate ratione at, odio facere non expedita!
-                
-            </div>
-        </div>
     </main>
     <script src="script.js"></script>
 </body>
--- TP5/script.js
+++ TP5/script.js
@@ -17,23 +17,45 @@ function getMovies(url){
     })
 }
 
-function showMovies(data){
+function showMovies(data ){
     main.innerHTML = '';
     data.forEach(movie => {
         const {title, poster_path ,vote_average,overview} = movie;
-        const movieEl = document.createElement('div');
-        movieEl.classList.add('movie');
-        movieEl.innerHTML = `   <img src="${IMGPATH+poster_path }" alt="">
-        <div class="movie-info">
-            <h3>${title}</h3>
-            <span class="${getcolor(vote_average)}">${vote_average}</span>
-        </div>
-        <div class="overview">
-            <h3>overview</h3>
-            ${overview}
-            
-        </div>`
-        main.appendChild(movieEl);
+
+//!Creation des balise
+const movieEl = document.createElement('div');
+const img = document.createElement('img');
+const divinfo = document.createElement('div');
+const h3 = document.createElement('h3');
+const span = document.createElement('span');
+const ovserview = document.createElement('div')
+const h3info = document.createElement('h3')
+
+
+//!attribution des class 
+img.setAttribute('class','img');
+divinfo.setAttribute('class', 'movie-info');
+h3.setAttribute('class', 'lh3');
+h3.innerText = title;
+span.setAttribute('class', getcolor(vote_average));
+span.innerHTML = vote_average;
+ovserview.setAttribute('class', 'overview');
+movieEl.setAttribute('class', 'movie');
+h3info.setAttribute('class', 'h3info');
+img.setAttribute('src', IMGPATH+poster_path)
+ovserview.textContent = overview;
+
+//!Chargement de parrents
+
+
+main.appendChild(movieEl);
+movieEl.appendChild(img)
+movieEl.appendChild(divinfo)
+movieEl.appendChild(ovserview)
+
+ovserview.appendChild(h3info);
+divinfo.appendChild(h3)
+divinfo.appendChild(span)
     })
 }
 
@@ -54,8 +76,28 @@ form.addEventListener('submit',(e) =>{
     if (searchTerm){
         getMovies(SEARCHAPI+ searchTerm)
     }
+
 })
 
+    
+
+
+
+
+// const {title, poster_path ,vote_average,overview} = movie;
+// const movieEl = document.createElement('div');
+// movieEl.classList.add('movie');
+// movieEl.innerHTML = `   <img src="${IMGPATH+poster_path }" alt="">
+// <div class="movie-info">
+//     <h3>${title}</h3>
+//     <span class="${getcolor(vote_average)}">${vote_average}</span>
+// </div>
+// <div class="overview">
+//     <h3>overview</h3>
+//     ${overview}
+    
+// </div>`
+// main.appendChild(movieEl);
 
 
 
